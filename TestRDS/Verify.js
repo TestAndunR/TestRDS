379edf9c64125b4eba9353fbf9fc60f8b7c1594d
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-04-20 12:16:31 +0530

    new lambda

--- TestRDS/Verify.js
+++ TestRDS/Verify.js
@@ -4,7 +4,8 @@ let SL_AWS = require('slappforge-sdk-aws');
 const rds = new SL_AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
 
-
+	email = event.email;
+	console.log(email);
 	// You can pass the existing connection to this function.
 	// A new connection will be created if it's not present as the third param 
 	// You must always end/destroy the DB connection after it's used

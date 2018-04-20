83cb2aa8ddd3a68f7645f27abc518ce186d47e3f
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-04-20 12:13:38 +0530

    new lambda

--- TestRDS/Verify.js
+++ TestRDS/Verify.js
@@ -3,30 +3,26 @@ let connectionManager = require('./ConnectionManager');
 let SL_AWS = require('slappforge-sdk-aws');
 const rds = new SL_AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
-	console.log(event);
-	let email = event.email;
-	console.log(email);
 
-	let inserts = [event.email];
 
 	// You can pass the existing connection to this function.
 	// A new connection will be created if it's not present as the third param 
 	// You must always end/destroy the DB connection after it's used
 	rds.query({
-  instanceIdentifier: 'TestRDS',
-  query: 'INSERT INTO users(email)VALUES(?);',
-  inserts: inserts
+		instanceIdentifier: 'TestRDS',
+		query: 'SELECT user WHERE email=(?);',
+		inserts: email
 }, function (error, results, connection) {
-		if (error) {
-			response = error;
-			console.log("Error occurred");
-			throw error;
-		} else {
-			response = "Successfully added a new entry";
-			console.log(results);
-		}
-		connection.end();
-		callback(null,response);
-		
-	});
+	if (error) {
+		console.log("Error occurred");
+		throw error;
+	} else {
+		console.log("Success")
+		console.log(results);
+	}
+
+	connection.end();
+});
+
+callback(null, 'Successfully executed');
 }
